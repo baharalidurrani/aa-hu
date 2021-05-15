aa99f38f89bb24a93098bdce59bc5c03feb0fe25
baharalidurrani <baharalidurrani@gmail.com>
2021-05-15 23:32:09 +0500

    failing test adde

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,5 +1,9 @@
 import { addFn } from "./main";
 
-it("add test", () => {
+it("add test that passes", () => {
   expect(addFn(1, 3)).toBe(4);
+});
+
+it("add function test that fails", () => {
+  expect(addFn(9, 1)).toBe(9);
 });
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
 export function addFn(x: number, y: number) {
   return x + y;
 }
-
-console.log(addFn(2, 3));
